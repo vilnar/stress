f9e97b134268131406d4eb1f1f57841632c8a662
klirichek <alexey@manticoresearch.com>
2019-05-13 12:15:57 +0700

    Copyright comment in sphinxapi.go

--- sphinxapi.go
+++ sphinxapi.go
@@ -1,3 +1,15 @@
+//
+// Copyright (c) 2019, Manticore Software LTD (http://manticoresearch.com)
+// All rights reserved
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License. You should have
+// received a copy of the GPL license along with this program; if you
+// did not, you can find it at http://www.gnu.org/
+//
+
+// This is subset of go-sdk used just to push searches using agent flavour.
+
 package main
 
 import (
